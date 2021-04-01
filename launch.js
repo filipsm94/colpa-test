815f279721274145beb8dfe86d44785e20143c87
Filipsm94 <filipsm94@gmail.com>
2021-04-01 14:36:13 -0500

    configurancion de despliegue en heroku

---
+++ launch.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/colaptriaTest'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname,'/dist/colaptriaTest/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('app ready');
+});
