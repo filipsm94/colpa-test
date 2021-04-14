4bf8bbc1a8cebb5d5e8a038ace134a4dabdd3933
Filipsm94 <filipsm94@gmail.com>
2021-04-14 11:44:30 -0500

    .

--- src/app/store/reducers/product/product.reducer.ts
+++ src/app/store/reducers/product/product.reducer.ts
@@ -1,8 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { IProductModel } from 'src/app/shared/models/products.model';
-import { IUserModel } from 'src/app/shared/models/user.model';
 import { defineProductSelected, deleteProductSelected } from '../../actions/product.actions';
-import { defineUser, deleteUser } from '../../actions/user.actions';
 
 export const colpatriaProduct: IProductModel = {
     accountAmountAvaliable: 0,
